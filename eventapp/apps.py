a33383897ce321e6b032e98bb173e9b99c101939
Anandhu <anandhuma468@gmail.com>
2026-07-30 08:07:48 +0530

    Auto-fix legacy image paths on startup

--- eventapp/apps.py
+++ eventapp/apps.py
@@ -1,5 +1,21 @@
 from django.apps import AppConfig
+from django.db.utils import OperationalError, ProgrammingError
 
 
 class EventappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'eventapp'
+
+    def ready(self):
+        try:
+            from .models import Event
+            updated_count = 0
+            for event in Event.objects.all():
+                if event.img and event.img.name.startswith('pic/'):
+                    event.img.name = event.img.name.replace('pic/', 'img/', 1)
+                    event.save()
+                    updated_count += 1
+            if updated_count > 0:
+                print(f"Successfully fixed {updated_count} image paths from pic/ to img/!")
+        except (OperationalError, ProgrammingError):
+            pass
